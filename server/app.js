9177b95813c8999f3e0e2e301fb4d631badd2274
safwat-fathi <safwat.rashwa@gmail.com>
2020-10-26 17:01:50 +0200

    test: search route

--- server/app.js
+++ server/app.js
@@ -8,3 +8,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // add routes
+const urlRoutes = require("./api/routes/url.route");
+
+app.use("/", urlRoutes);
+
+module.exports = app;
